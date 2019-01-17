f372170558f50d3de1a7fef7516089f371d5174f
furysun <fury.sun.main@gmail.com>
2019-01-17 20:18:33 +0200

    commit

--- app/index.js
+++ app/index.js
@@ -1,10 +1,4 @@
-// import {functionOne} from "./js/fileOne";
 
 import {displayIndexPage} from "./src/indexPage";
 
 document.getElementById("content").innerHTML = displayIndexPage();
-
-window.onhashchange = function () {
-    var oldHash = window.location.hash;
-    console.log(oldHash);
-};
